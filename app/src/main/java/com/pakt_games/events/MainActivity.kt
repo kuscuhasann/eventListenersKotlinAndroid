dc464ce8b16e694931fd04c7d1fb37d8ef2e85fb
kuscuhasann <kuscuhasann97@gmail.com>
2022-05-31 23:42:12 +0300

    Listener Çalışması tamamlandı.

--- app/src/main/java/com/pakt_games/events/MainActivity.kt
+++ app/src/main/java/com/pakt_games/events/MainActivity.kt
@@ -1,29 +1,87 @@
 package com.pakt_games.events
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.KeyEvent
+import android.view.MotionEvent
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.pakt_games.events.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bindingMain: ActivityMainBinding
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindingMain = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingMain.root)
 
+        registerForContextMenu(bindingMain.buttonMenuOpener)
+
+        onClickEvent()
+        onLongClickEvent()
+        setOnKeyListenerEvent()
+        setOnFocusListenerEvent()
+        setOnTouchListenerEvent()
+
+        bindingMain.editTextTest.requestFocus()
+
+    }
+
+    override fun dispatchTouchEvent(event: MotionEvent?): Boolean {
+        println("dispatchTouchEvent çalıştı")
+        return super.dispatchTouchEvent(event)
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.menu_example, menu)
+    }
+
+
+    fun onClickEvent() {
         bindingMain.buttonTest.setOnClickListener {
             println("SetONClickListener Tıklandı")
         }
-
+    }
+    fun onLongClickEvent() {
         bindingMain.buttonTest.setOnLongClickListener(object: View.OnLongClickListener {
             override fun onLongClick(v: View?): Boolean {
                 println("LONG CLİCK Tıklandı")
                 return true  }
         })
-
-        bindingMain.buttonTest.setOnFocusChangeListener { view, b -> println("On focus change listener tetiklendi") }
-
+    }
+    fun setOnKeyListenerEvent() {
+        bindingMain.buttonTest.setOnKeyListener(object : View.OnKeyListener {
+            override fun onKey(v: View?, keyCode: Int, event: KeyEvent?): Boolean {
+                return if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                    println("onKey için Entera tıklanıldı.")
+                    true
+                } else false
+            }
+        })
+    }
+    fun setOnFocusListenerEvent() {
+        bindingMain.editTextTest.setOnFocusChangeListener { _, b -> println("On focus change listener tetiklendi") }
+    }
+    @SuppressLint("ClickableViewAccessibility")
+    fun setOnTouchListenerEvent() {
+        bindingMain.buttonTest.setOnTouchListener(View.OnTouchListener { view, motionEvent ->
+            when (motionEvent.action){
+                MotionEvent.ACTION_DOWN -> {
+                    println(" setOnTouchListener Buttona basmaya başladınız")
+                }
+                MotionEvent.ACTION_UP -> {
+                    println(" setOnTouchListener Buttona basmayı bıraktınız")
+                }
+            }
+            return@OnTouchListener true
+        })
 
     }
 }
